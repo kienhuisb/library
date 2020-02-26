4b01e355d04a21e14dbb353f0e25a6f38cf09801
Michael Fross <michael@fross.org>
2020-02-26 16:18:05 -0600

    Added humanReadable() method to the Format class.  Updated the version of the library.

--- src/main/java/org/fross/library/Format.java
+++ src/main/java/org/fross/library/Format.java
@@ -35,5 +35,28 @@ public class Format {
 
 		return (String.valueOf(df.format(num)));
 	}
+	
+	
+	/**
+	 * HumanReadable(): Take a number in Bytes and return a more human readable format
+	 * 
+	 * Reference:
+	 * https://stackoverflow.com/questions/3758606/how-to-convert-byte-size-into-human-readable-format-in-java
+	 * 
+	 * @param bytes
+	 * @return
+	 */
+	public static String humanReadable(long bytes) {
+		String[] descriptor = { "K", "M", "G", "T", "P", "E" };
+		int unit = 1000; // As opposed to 1024
+
+		if (bytes < unit) {
+			return bytes + "  B";
+		}
+
+		int exp = (int) (Math.log(bytes) / Math.log(unit));
+		String pre = descriptor[exp - 1];
+		return String.format("%.3f %sB", bytes / Math.pow(unit, exp), pre);
+	}
 
 }
