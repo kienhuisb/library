da49366f259eb1c922b6e10f940c9e6c545c0b7b
Michael Fross <michael@fross.org>
2020-04-05 20:41:06 -0500

    Added a Date class to put calendar related methods.  Today it just has a few to return current day, month, year.

---
+++ src/main/java/org/fross/library/Date.java
@@ -0,0 +1,38 @@
+package org.fross.library;
+
+public class Date {
+
+	/**
+	 * getCurrentMonth(): Return an integer value of the current month
+	 * 
+	 * @return
+	 */
+	public static int getCurrentMonth() {
+		java.util.Calendar jc = java.util.Calendar.getInstance();
+		int month = jc.get(java.util.Calendar.MONTH) + 1;
+		return month;
+	}
+
+	/**
+	 * getCurrentDay(): Return an integer value of the current day
+	 * 
+	 * @return
+	 */
+	public static int getCurrentDay() {
+		java.util.Calendar jc = java.util.Calendar.getInstance();
+		int year = jc.get(java.util.Calendar.DAY_OF_MONTH);
+		return year;
+	}
+
+	/**
+	 * getCurrentYear(): Return an integer value of the current year
+	 * 
+	 * @return
+	 */
+	public static int getCurrentYear() {
+		java.util.Calendar jc = java.util.Calendar.getInstance();
+		int year = jc.get(java.util.Calendar.YEAR);
+		return year;
+	}
+
+}
